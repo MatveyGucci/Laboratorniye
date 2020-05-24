65318f6dfd531f4ce0721ed33735ade2552fe78a
MatveyGucci <65868615+MatveyGucci@users.noreply.github.com>
2020-05-25 00:18:27 +0300

    Laba 5

--- Laba5/src/com/company/Main.java
+++ Laba5/src/com/company/Main.java
@@ -1,40 +1,41 @@
-package com.company;
-
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class Main {
-
-    public static void main(String[] args) {
-    }
-}
-    class product{
-        String name;
-        int price;
-        public product(String name, int price)
-        {
-            this.name = name;
-            this.price = price;
-        }
-    }
-    class cashregister {
-        public ArrayList<String> scanproducts ( ArrayList<product> basket){
-            ArrayList<String> cheque = new ArrayList<String>();
-            ArrayList<product> reservbasket = new ArrayList<product>();
-            reservbasket.addAll(basket);
-
-            for (product ror : basket) {
-                int o =0;
-                    while (reservbasket.contains(ror))
-                    {
-                        reservbasket.remove(ror);
-                        o++;
-                    }
-                    if (o !=0)
-                    cheque.add(ror.name + " " + o + " " + ror.price*o+"грн");
-                }
-            Collections.sort(cheque);
-            return cheque;
-        }
-    }
-
+ package com.company;
+ 
+ import java.util.ArrayList;
+ import java.util.Collections;
+ 
+ public class Main {
+ 
+     public static void main(String[] args) {
+     }
+ }
+     class product{
+         String name;
+         int price;
+         public product(String name, int price)
+         {
+             this.name = name;
+             this.price = price;
+         }
+     }
+     class cashregister {
+         public ArrayList<String> scanproducts ( ArrayList<product> basket){
+             ArrayList<String> cheque = new ArrayList<String>();
+             ArrayList<product> reservbasket = new ArrayList<product>();
+             reservbasket.addAll(basket);
+ 
+             for (product ror : basket) {
+                 int o =0;
+                     while (reservbasket.contains(ror))
+                     {
+                         reservbasket.remove(ror);
+                         o++;
+                     }
+                     if (o !=0)
+                     cheque.add(ror.name + " " + o + " " + ror.price*o+"грн");
+                 }
+             Collections.sort(cheque);
+             return cheque;
+         }
+     }
+ 
+ 
